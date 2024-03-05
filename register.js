f7e8843a171ca4f4d01a775e5e949c0300ef7b3e
GuianneXandler <guiannexandlerg@gmail.com>
2024-03-05 16:22:39 +0800

    register.html

---
+++ register.js
@@ -0,0 +1,23 @@
+console.log("Guianne Xandler");
+console.error("GXG");
+console.warn("Mag ride na ulit tayo");
+//variables
+// string = "Wordss" , "wowowo" , "123"
+// number = 123, 434e, 2398.12
+//boolean = true , false
+//object =["apple" , "banana" ], ["1, 2 , 3 , 4"],["apple" , "1"]
+//console.log(typeof(["apple" , banana"]))
+//Declaration of variables
+let name = "Guianne" ,
+lastbname = "Birondo" ,
+age = 10,
+isProf = true;
+hobbies = ["watch tv", "play video games"],
+run = function (){
+    console.log("I'm running")
+}
+console.log(name, age, isProf, hobbies);
+console.log(tupeof run);
+console.table(hobbies);
+console.log("My name is" + name " " + lasrtname);
+console.log(`My name is ${name} ${lastname}`)
